7c6568007bff5851719ba7415d6983bd09c2e7ed
michaelspringer250 <mspringer251@gmail.com>
2021-07-26 15:05:45 -0500

    Added support for extra game modes (except robot turn)

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -854,4 +854,85 @@
             <translation type="obsolete">Welcome to tic tac toe!</translation>
         </message>
     </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Initialize Game/Game Choice/Choice</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="unfinished">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="unfinished">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="unfinished">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="unfinished">exit/stop/quit</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="unfinished">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="unfinished">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="unfinished">, / or </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="unfinished">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="unfinished"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="unfinished">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="unfinished">I did not understand. </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="unfinished">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="unfinished">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
 </TS>
