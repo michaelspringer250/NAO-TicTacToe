1ceaabc6585441900bd6145d732024fc2156a595
Lane Simpson <lsimpson1040@gmail.com>
2021-07-29 16:45:57 -0500

    converted pickle files

--- behavior_1/filetest.py
+++ behavior_1/filetest.py
@@ -1,14 +1,34 @@
 import pickle
+import enum
+
+class PType(enum.Enum):
+    P1 = 1
+    P2 = 2
 
 dictionary_data = {"a": 1, "b": 2}
 
-a_file = open("data.pkl", "wb")
-pickle.dump(dictionary_data, a_file)
-a_file.close()
+# a_file = open("regular.pkl", "wb")
+# pickle.dump(dictionary_data, a_file)
+# a_file.close()
 
-a_file = open("data.pkl", "rb")
+a_file = open("reversewild.pkl", "rb")
 output = pickle.load(a_file)
-print(output)
 
-t1 = t2 = t3 = 1
-print(t1, t2, t3)
+d = dict()
+
+for key in output:
+    items = []
+    for item in output[key]:
+        winner = None
+        if item[0] == PType.P1:
+            winner = 1
+        if item[0] == PType.P2:
+            winner = 2
+        items.append((winner, item[1], item[2], item[3]))
+        d[key] = items
+
+#print(output)
+print(d)
+#print(output["---------"])
+#print(d["---------"])
+
